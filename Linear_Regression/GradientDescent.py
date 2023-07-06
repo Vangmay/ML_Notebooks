ebd1113825ede430954799982d72b1fa49664f8c
Vangmay Sachan <78148958+Vangmay@users.noreply.github.com>
2023-07-06 14:52:18 +0530

    Comments on gradient descent

--- Linear_Regression/GradientDescent.py
+++ Linear_Regression/GradientDescent.py
@@ -7,28 +7,38 @@
 
 
 import random 
-f = {i : (i*2 + 3) for i in range(0,1000)}
+f = {i : ((i*5) + 3) for i in range(0,1000)}
 points = f 
 
 
 def Loss(Yintercept, Slope ,points):
+    """
+    This function calculates the loss by the 
+    Summing the squares of differences between the true Y values and predicted Y values
+    """
     totalError = 0 
     for i in range(0,len(points)):
         totalError += (points[i] - ((Slope * i) + Yintercept)) ** 2    
     return (totalError)
 
 def ErrorAtAPoint(Points, i, Yintercept, Slope):
+    """
+    This function returns the differences between the TrueY and PredictedY needed for derivative calculation
+    """
     TrueX = i 
     TrueY = Points[i]
     PredictedY = (Slope * i) + Yintercept
     return (TrueY - PredictedY)
 
 def Step(Yintercept, Slope, points, learningRate):
+    """
+    This function calculates the step size and updates the values of Yintercept and Slope
+    """
     dlDm , dlDs = 0,0  
     N = float(len(points))
     for i in range(0,len(points)):
         dlDm = -2 * ErrorAtAPoint(points, i, Yintercept, Slope) # Relates how the Loss changes as the intercept changes
-        dlDs = -2 * i * ErrorAtAPoint(points, i, Yintercept, Slope ) 
+        dlDs = -2 * i * ErrorAtAPoint(points, i, Yintercept, Slope ) # Relates how the Loss changes as the Slope changes
     newIntercept = Yintercept - (dlDm * learningRate)
     newSlope = Slope - (dlDs * learningRate)
     return [newIntercept, newSlope]
@@ -43,7 +53,7 @@ def Start():
     learningRate = 0.0000005
     Yintercept = 0
     Gradient = 0
-    iterations = 20000
+    iterations = 200000
     [b,m] = GradientDescent(Yintercept, Gradient, iterations, points, learningRate)
     print(f"Y = {m}X + {b}")
 
